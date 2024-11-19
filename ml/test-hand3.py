72b2a584a1038718b3df7f2a01727a1470045835
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-11-19 07:47:40 -0600

    apuntes data cleaning

--- ml/test-hand3.py
+++ ml/test-hand3.py
@@ -11,7 +11,6 @@ connection_drawing_spec = mp_drawing.DrawingSpec(color=(0, 0, 0), thickness=2, c
 
 cap = cv2.VideoCapture(0)
 
-# Parámetros personalizables para la ventana de texto
 mensaje = "Gesto Detectado!"
 color_texto = (255, 255, 255)  # blanco
 tamano_texto = 1  # Tamaño de texto
@@ -33,20 +32,21 @@ with mp_hands.Hands(
         gesture_detected = False  # Reiniciar detección en cada frame
 
         if results.multi_hand_landmarks:
-            # Comprobar si se detectan ambas manos para formar el gesto
+
             if len(results.multi_hand_landmarks) == 2:
                 hand1 = results.multi_hand_landmarks[0]
                 hand2 = results.multi_hand_landmarks[1]
 
-                # Posiciones de los puntos de interés
+                
                 index1 = hand1.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
                 thumb1 = hand1.landmark[mp_hands.HandLandmark.THUMB_TIP]
                 index2 = hand2.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
                 thumb2 = hand2.landmark[mp_hands.HandLandmark.THUMB_TIP]
 
-                # Condiciones para detectar el gesto
-                if (abs(index1.x - index2.x) < 0.1 and abs(index1.y - index2.y) < 0.1 and
-                    abs(thumb1.x - thumb2.x) < 0.1 and abs(thumb1.y - thumb2.y) < 0.1):
+                # Condiciones para detectar el gesto (indices y pulgares juntos)
+                if (abs(index1.x - index2.x) < 0.1 and abs(index1.y - index2.y) < 0.1 
+                and abs(thumb1.x - thumb2.x) < 0.1 and abs(thumb1.y - thumb2.y) < 0.1 
+                and abs(index1.x - thumb1.x) < 0.15 and abs(index1.y - thumb1.y) < 0.15):
                     gesture_detected = True
 
             # Dibujar las manos en el frame
@@ -60,24 +60,21 @@ with mp_hands.Hands(
 
         # Mostrar el texto personalizado si se detecta el gesto
         if gesture_detected:
-            # Crear una imagen en blanco para el mensaje (200x400 píxeles)
             mensaje_frame = np.zeros((200, 400, 3), dtype=np.uint8)
 
-            # Tamaño y posición del texto
-            posicion_texto = (50, 100)  # Ajusta la posición según el tamaño de la ventana y el texto
+            posicion_texto = (50, 100)  
 
             # Agregar texto a la ventana de mensaje
             cv2.putText(mensaje_frame, mensaje, posicion_texto, cv2.FONT_HERSHEY_SIMPLEX, tamano_texto, color_texto, grosor_texto)
 
-            # Mostrar la ventana del mensaje
             cv2.imshow("Mensaje", mensaje_frame)
 
         else:
-            # Cerrar la ventana del mensaje si el gesto no está presente
             if cv2.getWindowProperty("Mensaje", cv2.WND_PROP_VISIBLE) >= 1:
                 cv2.destroyWindow("Mensaje")
 
-        # Mostrar el frame principal
+        
+        # Frame Principal
         cv2.imshow("Hand Detection", frame)
 
         # Presiona 'q' para salir
